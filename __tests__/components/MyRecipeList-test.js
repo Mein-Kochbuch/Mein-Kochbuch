a4020ea611a40bfe8ac5f12aa2db94cf57ccb38c
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-10-24 13:17:32 +0200

    Add MyRecipeList-test Unit Test

---
+++ __tests__/components/MyRecipeList-test.js
@@ -0,0 +1,42 @@
+import 'react-native';
+import React from 'react';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+import MyRecipesList from "../../src/components/MyRecipesList";
+import MultiListItem from "../../src/components/MultiListItem";
+import CookbookListItem from "../../src/components/CookbookListItem";
+
+jest.mock("../../src/components/MultiListItem", () => {
+    return (props) => {
+        return <div {...props}/>
+    }
+});
+
+jest.mock("../../src/components/CookbookListItem", () => {
+    return (props) => {
+        return <div {...props}/>
+    }
+});
+
+const cookbooks = [{cookbook: "1", pk: "1"}, {cookbook: "2", pk: "2"}]
+
+it('MainMenu Test', () => {
+    const component = renderer.create(
+        <MyRecipesList cookbooks={cookbooks}/>
+    );
+
+    expect(component.toJSON()).toMatchSnapshot()
+    expect(component.toJSON().children.length).toBe(5)
+
+    const testInstance = component.root
+
+    expect(testInstance.findAllByType(MultiListItem)[0].props.title).toBe("All Recipes")
+
+    expect(testInstance.findAllByType(MultiListItem)[1].props.title).toBe("Favorites")
+
+    expect(testInstance.findAllByType(CookbookListItem)[0].props.cookbook).toStrictEqual({cookbook: "1", pk: "1"},)
+
+    expect(testInstance.findAllByType(CookbookListItem)[1].props.cookbook).toStrictEqual({cookbook: "2", pk: "2"})
+
+})
