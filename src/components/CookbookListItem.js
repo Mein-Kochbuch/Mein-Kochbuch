71898ccfb6b62b50ad1b736d92788d477078b707
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-10-30 22:13:55 +0200

    Add CookbookImage to CookBookListItem

--- src/components/CookbookListItem.js
+++ src/components/CookbookListItem.js
@@ -6,17 +6,32 @@ import {useColorScheme} from "react-native-appearance";
 export default function CookbookListItem({cookbook}) {
 
     const isDarkMode = useColorScheme() === 'dark';
+    const imageSource = cookbook.thumbnail ? {uri: cookbook.thumbnail} : require("../../resources/platzhalter.png")
 
     return (
         <StyledTouchableOpacity isDarkMode={isDarkMode}>
-            <StyledTest>
+            <StyledImage source={imageSource}/>
+            <StyledText numberOfLines={3} ellipsizeMode={"clip"}>
                 {cookbook.name}
-            </StyledTest>
+            </StyledText>
         </StyledTouchableOpacity>
     )
 }
 
-const StyledTest = styled.Text`
+const StyledImage = styled.Image`
+  height: 100px;
+  width: 100px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  background-color: #68a0cf;
+  border-radius: 10px;
+  border-width: 1px;
+  border-color: #fff;
+`
+
+const StyledText = styled.Text`
+  max-width: 70%;
+  margin-left: 12px;
   text-align: center;
   font-size: 30px;
 `
@@ -26,6 +41,7 @@ const StyledTouchableOpacity = styled.TouchableOpacity`
   margin: 12px;
   display: flex;
   align-items: center;
+  flex-direction: row;
   border-style: solid;
   border-color: ${props => props.isDarkMode ? Colors.lighter : Colors.darker};
   border-radius: 10px;
