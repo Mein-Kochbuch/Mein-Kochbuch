e2857fd4fc348c307b1821876ee036684baa0241
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-11-04 22:36:40 +0100

    Add Basic RecipeDetailsImageGallery

---
+++ src/components/recipes/details/RecipeDetailsImageGallery.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components/native";
+
+export default function RecipeDetailsImageGallery({images}) {
+    const imageSource = images?.length > 0 ? {uri: images[0].image} : require("../../../../resources/platzhalter.png")
+
+    return (
+        <ImageGallery>
+            <ImageStyled source={imageSource}/>
+        </ImageGallery>
+    )
+}
+
+const ImageGallery = styled.View`
+  margin: 12px;
+  height: 300px;
+`
+
+const ImageStyled = styled.Image`
+  width: 100%;
+  height: 100%;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  background-color: #68a0cf;
+  border-radius: 10px;
+  border-width: 1px;
+  border-color: #fff;
+`
