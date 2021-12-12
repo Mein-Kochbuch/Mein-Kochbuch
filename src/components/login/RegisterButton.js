e5b4d8c70fc9d1b3dbb7dbe41f71a444570e2b43
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-12-12 02:25:25 +0100

    Add RegisterButton.js

---
+++ src/components/login/RegisterButton.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components/native/dist/styled-components.native.esm";
+import {Colors} from "react-native/Libraries/NewAppScreen";
+import {useColorScheme} from "react-native-appearance";
+
+export default function RegisterButton() {
+
+    const isDarkMode = useColorScheme() === 'dark';
+
+    return (
+        <StyledButton
+            isDarkMode={isDarkMode}
+            onPress={() => {
+            }}>
+            <StyledText>
+                Register
+            </StyledText>
+        </StyledButton>
+    )
+}
+
+const StyledText = styled.Text`
+  text-align: center;
+  font-size: 30px;
+`
+
+const StyledButton = styled.TouchableOpacity`
+  margin: 12px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 80%;
+  min-width: 300px;
+  border-style: solid;
+  border-color: ${props => props.isDarkMode ? Colors.lighter : Colors.darker};
+  border-radius: 10px;
+  border-width: 2px;
+`
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -3,15 +3,13 @@ import {View} from "react-native";
 import Header from "./Header";
 import styled from "styled-components/native";
 import LoginForm from "../login/LoginForm";
-import {Colors} from "react-native/Libraries/NewAppScreen";
-import {useColorScheme} from "react-native-appearance";
 import {AuthContext} from "../../context/AuthProvider";
 import ErrorText from "../../utils/ErrorText";
+import RegisterButton from "../login/RegisterButton";
 
 export default function LoginPage() {
 
     const [loginError, setLoginError] = useState()
-    const isDarkMode = useColorScheme() === 'dark';
     const {login} = useContext(AuthContext)
 
     const onSubmit = (credentials) => {
@@ -29,12 +27,7 @@ export default function LoginPage() {
             <ErrorText text={loginError}/>
             <LoginForm onSubmit={onSubmit}/>
             <View>
-                <RegisterButton isDarkMode={isDarkMode} onPress={() => {
-                }}>
-                    <StyledText>
-                        Register
-                    </StyledText>
-                </RegisterButton>
+                <RegisterButton/>
             </View>
         </StyledView>
     )
@@ -46,21 +39,4 @@ const StyledView = styled.View`
   flex-direction: column;
 `
 
-const StyledText = styled.Text`
-  text-align: center;
-  font-size: 30px;
-`
 
-const RegisterButton = styled.TouchableOpacity`
-  margin: 12px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  width: 80%;
-  min-width: 300px;
-  border-style: solid;
-  border-color: ${props => props.isDarkMode ? Colors.lighter : Colors.darker};
-  border-radius: 10px;
-  border-width: 2px;
-`
