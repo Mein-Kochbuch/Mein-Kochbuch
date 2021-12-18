d1c33eea722e93418a9979647834104b37576f51
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-12-18 13:42:02 +0100

    Add LoginForm Link press tests

--- __tests__/components/login/LoginForm-test.js
+++ __tests__/components/login/LoginForm-test.js
@@ -3,16 +3,21 @@ import renderer from "react-test-renderer";
 import LoginForm from "../../../src/components/login/LoginForm";
 import {useColorScheme} from "react-native-appearance";
 import {fireEvent, render} from "@testing-library/react-native";
+import { Linking } from "react-native";
 
 jest.mock("../../../src/components/recipes/RecipeListItem", () => {
     return (props) => {
         return <div {...props}/>
     }
-});
+})
 
 jest.mock("react-native-appearance", () => ({
     useColorScheme: jest.fn(),
-}));
+}))
+
+jest.mock("react-native/Libraries/Linking/Linking", () => ({
+    openURL: jest.fn(() => Promise.resolve("mockResolve")),
+}))
 
 
 describe('LoginForm Test', () => {
@@ -46,5 +51,27 @@ describe('LoginForm Test', () => {
 
         fireEvent(getByText('Login'), 'onPress');
         expect(onSubmit).toBeCalledWith({username: "testMail", password: "testPassword"})
+    })
+
+    it('LoginForm Privacy Link press', () => {
+        useColorScheme.mockReturnValueOnce('dark');
+
+        const {getByText} = render(
+          <LoginForm/>
+        );
+
+        fireEvent(getByText(' Privacy Policy '), 'onPress');
+        expect(Linking.openURL).toBeCalledWith("https://mein-kochbuch.org/privacypolicy/")
+    })
+
+    it('LoginForm AGB Link press', () => {
+        useColorScheme.mockReturnValueOnce('dark');
+
+        const {getByText, ... component} = render(
+          <LoginForm/>
+        );
+
+        fireEvent(getByText(' AGBs '), 'onPress');
+        expect(Linking.openURL).toBeCalledWith("https://mein-kochbuch.org/agb/")
     })
 })
